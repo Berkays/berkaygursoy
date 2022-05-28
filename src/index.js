bba1802d2d7a5430ac40d4d0742dfedfeab497a5
berkay <berkaygursoy@gmail.com>
2022-05-28 06:11:16 +0300

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+window.addEventListener('load', function () {
+    const path = window.location.pathname.slice(1);
+    if (path != "") {
+        const element = this.document.body.querySelector(`a[data-href=${path}`);
+        if (element)
+        {
+            element.classList.add('active');
+            element.setAttribute('href', '#');
+        }
+    }
+});
