0e62047a63ed4d485b0b6e1d1eac3fb5ac84823c
berkay <berkaygursoy@gmail.com>
2022-05-29 04:28:37 +0300

    Build#4

--- 404.98ad8412.js
+++
@@ -1,2 +0,0 @@
-window.addEventListener("load",(function(){let t=window.location.pathname.slice(1);""==t&&(t="home");const e=this.document.body.querySelector(`a[data-href=${t}`);e&&(e.classList.add("active"),e.setAttribute("href","#"))}));
-//# sourceMappingURL=404.98ad8412.js.map
