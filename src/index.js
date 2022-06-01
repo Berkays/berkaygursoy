4a252fc096c3cac862ea99575889080ca21bc085
berkay <berkaygursoy@gmail.com>
2022-06-01 17:48:38 +0300

    Add active to mobile navbar

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
-import * as bootstrap from 'bootstrap';
+// import * as bootstrap from 'bootstrap';
+import { Collapse } from 'bootstrap';
 
 window.addEventListener('load', function () {
     let path = window.location.pathname.slice(1);
     if (path == '')
         path = 'home';
-    const element = this.document.body.querySelector(`a[data-href=${path}`);
+    const element = this.document.body.querySelectorAll(`a[data-href=${path}`);
     if (element) {
-        element.classList.add('active');
-        element.setAttribute('href', '#');
+        for (const el of element) {
+            el.classList.add('active');
+            el.setAttribute('href', '#');
+        }
     }
 });
 
 window.addEventListener('load', function () {
-
-    const width = window.innerWidth || document.documentElement.clientWidth ||
-        document.body.clientWidth;
-
     let textWrapper = document.querySelector('.ml10 .letters');
 
     if (!textWrapper)
         return;
-
-    if (width < 900) {
-        textWrapper.innerHTML = "Hi! <br/> I'm Berkay";
-        return;
-    }
 
     textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
@@ -35,8 +29,4 @@ window.addEventListener('load', function () {
             duration: 1000,
             delay: (el, i) => 350 + (40 * i)
         });
-
-    this.setTimeout(() => {
-        textWrapper.innerHTML = "Hi! I'm Berkay";
-    }, 2000);
 });
